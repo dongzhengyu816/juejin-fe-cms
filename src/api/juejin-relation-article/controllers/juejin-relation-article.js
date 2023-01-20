e2394b10af37c1b2a35bf8ed786e61c99f37ee99
董正宇 <dzy20010816@163.com>
2023-01-20 09:43:20 +0800

    feat: 添加相关文章

---
+++ src/api/juejin-relation-article/controllers/juejin-relation-article.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  juejin-relation-article controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::juejin-relation-article.juejin-relation-article');
---
+++ src/api/juejin-relation-article/services/juejin-relation-article.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * juejin-relation-article service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::juejin-relation-article.juejin-relation-article');
